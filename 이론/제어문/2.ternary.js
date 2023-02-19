c5f5335f9b14b329747b506832f41c0e3ca9711f
hae-on <solbi2004@naver.com>
2023-02-19 15:08:11 +0900

    제어문

---
+++ 이론/제어문/2.ternary.js
@@ -0,0 +1,8 @@
+// 삼항 조건 연산자 Ternary Operator
+// 조건식? 참인 경우 : 거짓인 경우
+
+let fruit = 'apple';
+fruit === 'apple' ? console.log('🍎') : console.log('🍊');
+
+let emoji = fruit === 'apple' ? '🍎' : '🥲';
+console.log(emoji);
---
+++ 이론/제어문/3.quiz.js
@@ -0,0 +1,11 @@
+// 퀴즈!
+let num = 2;
+// num의 숫자가 짝수이면 👍, 홀수라면 👎을 출력하도록
+// 1. if
+if (num % 2 === 0) console.log('👍');
+else console.log('👎');
+// 2. ternary
+num % 2 === 0 ? console.log('👍') : console.log('👎');
+
+let emoji = num % 2 === 0 ? '👍' : '👎';
+console.log(emoji);
