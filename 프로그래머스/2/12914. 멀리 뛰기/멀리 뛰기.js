b399a8a8091cb6cbebabcc6fa725b6299d83cde0
sᴏʟʙɪ ☔️ <solbi2004@naver.com>
2023-10-26 21:47:59 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = 0;
+    const dp = new Array(n).fill(0);
+    dp[0] = 1;
+    dp[1] = 2;
+    
+    for(let i=2; i<=n; i++){
+        dp[i] = (dp[i-2] + dp[i-1])%1234567;
+    }
+    
+    answer = dp[n-1]
+  
+    return answer;
+}
