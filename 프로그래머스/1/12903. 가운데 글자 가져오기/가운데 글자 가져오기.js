c070bc6eac409e9562a7da94ada9c60c3dce0676
sᴏʟʙɪ ☔️ <solbi2004@naver.com>
2024-05-13 13:50:51 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,10 +1,29 @@
 function solution(s) {
     let answer = '';
-    const strLen = s.length;
-    if(strLen % 2 !== 0){
-        answer = s[Math.floor(s.length / 2)];
+    if(s.length % 2 !== 0){
+        answer = s[Math.floor(s.length / 2)]
     } else {
-        answer = s[Math.floor(s.length / 2) - 1] + s[Math.floor(s.length / 2)];
+        answer = s.slice((s.length / 2) - 1, s.length / 2 + 1)
     }
     return answer;
 }
+
+//     if(s.length % 2 ===0){
+//     let even = s.length / 2;
+//     answer = s[even-1] + s[even];
+// } 
+// if(s.length % 2 === 1){
+//     let odd = Math.floor(s.length/2);
+//     answer = s[odd];
+// }
+
+// function solution(s) {
+//   let answer = '';
+//   const strLen = s.length;
+//   if(strLen % 2 !== 0){
+//       answer = s[Math.floor(s.length / 2)];
+//   } else {
+//       answer = s[Math.floor(s.length / 2) - 1] + s[Math.floor(s.length / 2)];
+//   }
+//   return answer;
+// }
