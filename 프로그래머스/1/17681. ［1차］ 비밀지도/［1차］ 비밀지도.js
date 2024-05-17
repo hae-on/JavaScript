9c0c49ef20076bbf303677b9d5330bdd5095291b
sᴏʟʙɪ ☔️ <solbi2004@naver.com>
2024-05-17 12:43:36 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,21 @@
+function solution(n, arr1, arr2) {
+    let answer = [];
+        
+    for(let i=0; i<n; i++){
+    let first = '0'.repeat(n-arr1[i].toString(2).length)+arr1[i].toString(2);
+    let second = '0'.repeat(n-arr2[i].toString(2).length)+arr2[i].toString(2);
+        
+        let tmp = '';
+        
+        for(let j=0; j<n; j++){
+            if (first[j] === '1' || second[j] === '1'){
+                tmp += '#'
+            } else if (first[j] === '0' || second[j] === '0'){
+                tmp += ' '
+            }
+        }
+        answer.push(tmp)
+    }
+  
+    return answer;
+}
