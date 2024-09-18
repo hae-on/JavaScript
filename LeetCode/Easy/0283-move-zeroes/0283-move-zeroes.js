294c9f6d488551c2bea048dbc09392565caa01b8
sᴏʟʙɪ ☔️ <solbi2004@naver.com>
2024-09-18 20:39:13 +0900

    [Easy] Title: 283. Move Zeroes, Time: 85 ms, Memory: 54.3 MB - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.js
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.js
@@ -3,19 +3,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let count = 0;
-    let index = 0;
-    while(nums.length >= index){
-        if(nums[index] === 0) {
-            nums.splice(index, 1);
-            count++;
-        } else {
-            index++;
-        }    
-    }
-    for(let i=0; i<count; i++){
-        nums.push(0);
-    }
+    nums.sort((a, b) => {
+        if(a === 0 && b !== 0) return 1;
+        if(a !== 0 && b === 0) return -1;
+        return 0;
+    })
 
     return nums;
 };
